17f225d64f8cab726e22d0ba44fae15fc9466e09
kanuj507 <kanuj507@gmail.com>
2015-06-29 19:17:54 +0530

    V-29/06/15

--- WS-temp-1/src/main/java/search/Find.java
+++ WS-temp-1/src/main/java/search/Find.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import constant.*;
 public class Find {
 	
-	  String[] aryLines = {};       
+	  String[] aryLines = {};   
 	public void FindById(String IdValue)
 	{
 	//	String id=null;
@@ -51,6 +52,10 @@ public class Find {
         }
 		buffer.close();
 	}
+		catch(NoSuchElementException e)
+		{
+			e.printStackTrace();
+		}
 		 catch (FileNotFoundException e) {
 		        // TODO Auto-generated catch block
 		        e.printStackTrace();
@@ -60,6 +65,11 @@ public class Find {
 		}
 		
 	System.out.println("Count : "+ count+"CountBuffer :"+countBuffer);
+	if(count>0)
+	{
+		System.out.println("Successfully found "+IdValue+" Element" );
+	}
+	
 	}
 	
 	public void FindByHref(String href)
@@ -111,8 +121,77 @@ public class Find {
 			e.printStackTrace();
 		}
 		
+		
 	System.out.println("Count : "+ count+"CountBuffer :"+countBuffer);
+	if(count>0)
+	{
+		System.out.println("Successfully found "+href+" Element" );
 	}
+	}
+	
+	
+	public void FindByText(String text)
+	{
+	//	String id=null;
+		  String line = "";
+		  System.out.println(line);
+		  String inputSearch = "id="+ "\""+text+"\"";
+		  double count = 0,countBuffer=0,countLine=0;
+		    String lineNumber = "";
+		
+		FileReader file_to_read;
+		try {
+			file_to_read = new FileReader(ConstantClass.FilePath);
+			 BufferedReader buffer = new BufferedReader(file_to_read);
+			 
+		
+		
+		
+		while((line = buffer.readLine()) != null)
+        {
+            countLine++;
+            //System.out.println(line);
+            String[] words = line.split(" ");
+
+            for (String word : words) {
+            	if( word.contains(inputSearch)){
+            //  if (word.equals(inputSearch)) {
+                count++;
+                countBuffer++;
+                System.out.println("Words :"+word);
+              }
+            }
+
+            if(countBuffer > 0)
+            {
+                countBuffer = 0;
+                lineNumber += countLine + ",";
+            }
+
+        }
+		buffer.close();
+	}
+		catch(NoSuchElementException e)
+		{
+			e.printStackTrace();
+		}
+		 catch (FileNotFoundException e) {
+		        // TODO Auto-generated catch block
+		        e.printStackTrace();
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	System.out.println("Count : "+ count+"CountBuffer :"+countBuffer);
+	if(count>0)
+	{
+		System.out.println("Successfully found "+text+" Element" );
+	}
+	
+	}
+	
+	
 	
 
 	public static void main(String[] args) {
