2fddcefeeaef54fa411cda59c869e621fb7aba7d
kanuj507 <kanuj507@gmail.com>
2015-06-30 19:18:50 +0530

    V-30/06/15

--- WS-temp-1/src/main/java/actions/Actions.js
+++ WS-temp-1/src/main/java/actions/Actions.js
@@ -6,13 +6,23 @@ function simulateClick() {
 		  var evt = document.createEvent("MouseEvents");
 		  evt.initMouseEvent("click", true, true, window,
 		    0, 0, 0, 0, 0, false, false, false, false, 0, null);
-		  var a = document.getElementById("gift-close"); 
+		  var a = document.getElementById("header"); 
 		  a.dispatchEvent(evt);      
 		}
 
 
 
-public void clickLink()
+public void clickLink(String IdValue)
 {
-	document.getElementById("gift-close").click();
+	document.getElementById("header").click();
 }
+
+
+var fun1 = function(name) {
+    print('Hi there from Javascript, ' + name);
+    return "greetings from javascript";
+};
+
+var fun2 = function (object) {
+    print("JS Class Definition: " + Object.prototype.toString.call(object));
+};
--- WS-temp-1/src/main/java/actions/Click.java
+++ WS-temp-1/src/main/java/actions/Click.java
@@ -1,18 +1,49 @@
 package actions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.script.*;
 public class Click {
 
-	
-	public void clickLink()
-	{
-		document.getElementById("gift-close").click();
-	}
+	//public static final String MY_JS
+    //= "print(\"Hello world!\\n\");"
+    //+ "var myFunction = function(x) { return x+3; };";
 	
 	
+	public static String header ;
+	public static final String MY_JS="document.getElementById("+header+").click();";
+public void clickLink(String IdValue)
+{
+	// document.getElementById("header").click();
+}
+
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException {
 		// TODO Auto-generated method stub
+		ScriptEngineManager factory = new ScriptEngineManager();
+		 // create a JavaScript engine
+		 ScriptEngine engine = factory.getEngineByName("JavaScript");
+		 // evaluate JavaScript code from String
+		 try {
+			//engine.eval(new java.io.FileReader("Actions.js"));
+			
+			engine.eval(new FileReader("Actions.js"));
+
+			Invocable invocable = (Invocable) engine;
+
+			Object result = invocable.invokeFunction("fun1", "Peter Parker");
+			System.out.println(result);
+			System.out.println(result.getClass());
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
+	
 
 }
